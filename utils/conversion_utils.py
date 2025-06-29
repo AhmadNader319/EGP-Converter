4cc85eb98c3648b5547823c271309c1de74e60a8
AhmadNader319 <ahmadnaderpro1@gmail.com>
2025-06-29 14:55:32 +0300

    Feat: Perform Exception Handling on utils/conversion_utils, issue(#6)

--- utils/conversion_utils.py
+++ utils/conversion_utils.py
@@ -1,3 +1,13 @@
 # --- Reformat date to American Date format
-def _format_date_component(component):
-     return f"0{component}" if component < 10 else str(component)
+def _format_date_component(component: int):
+    if (component == 0):
+        raise ValueError("Date component cannot be zero. Must be 1 or greater.")
+    if component < 1:
+        raise ValueError(f"Date component cannot be negative or zero. Received: {component}.")
+    try:
+        return f"0{component}" if component < 10 else str(component)
+    except TypeError as e:
+        raise ValueError(f"Input component must be an integer, got type {type(component).__name__}.") from e # Value error for user-friendly Explanation
+
+
+print(_format_date_component(0))
